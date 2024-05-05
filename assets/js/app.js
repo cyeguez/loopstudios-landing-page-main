ab14a6397fe29a411eed934df70da40913149b1f
Cesar Yeguez <Cesar.Yeguez@gmail.com>
2024-05-04 23:43:30 -0400

    Se genera estilos al menu modo mobile

---
+++ assets/js/app.js
@@ -0,0 +1,9 @@
+const btnMenu = document.getElementById("btn-menu");
+const btnClose = document.getElementById("btn-close");
+const nav = document.getElementById("nav");
+
+btnMenu.addEventListener("click", () => {
+  console.log("hola");
+});
+
+console.log(btnMenu)
