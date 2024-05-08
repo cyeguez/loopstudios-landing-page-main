0c1712a01db13120c294b04a3b0b09dd5bee9750
Cesar Yeguez <Cesar.Yeguez@gmail.com>
2024-05-07 23:02:50 -0400

    feact(Dar estilos en modo mobile y deskop a la sección de vr y galeria)

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,11 +1,13 @@
 const btnMenu = document.getElementById("btn-menu");
 const btnClose = document.getElementById("btn-close");
 const nav = document.getElementById("nav");
+const header = document.querySelector(".header");
 
 function toogleMenu() {
   btnMenu.classList.toggle("open-menu");
   btnClose.classList.toggle("open-menu");
   nav.classList.toggle("open-menu");
+  header.classList.toggle("open-menu");
 }
 
 btnMenu.addEventListener("click", () => {
